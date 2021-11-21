e1a1e2ee8ae33967dc6a9f2aed18e3335c2b554d
linhnmfx12988 <linhnmfx12988@funix.edu.vn>
2021-11-21 15:25:10 +0700

    Components Part 1

--- src/App.js
+++ src/App.js
@@ -1,24 +1,21 @@
-import React, { Component } from 'react';
-import { Navbar, NavbarBrand } from 'reactstrap';
-import Menu from './components/MenuComponent';
-import './App.css';
+import React, { Component } from "react";
+import { Navbar, NavbarBrand } from "reactstrap";
+import Menu from "./components/MenuComponent";
+import "./App.css";
 
-function App() {
-  return (
-    <div className="App">
-
-      <Navbar dark color="primary">
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <Navbar dark color="primary">
           <div className="container">
             <NavbarBrand href="#">Ristorante Con Fusion</NavbarBrand>
           </div>
         </Navbar>
         <Menu />
-    </div>
-  );
+      </div>
+    );
+  }
 }
 
 export default App;
-
- 
-
-
